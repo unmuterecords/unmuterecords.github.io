0faf0eda4b0b3f01e36ab6aad6220c81993f47d5
Ghrehh <gjburns9@hotmail.com>
2017-01-26 17:51:44 +0000

    fade in background on main page

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ function hoverColor(className, color){
   $(className).mouseenter(function(){
     $(this).animate({ color: color }, { duration: 200, queue: false});
   });
-  
+
   $(className).mouseleave(function(){
     $(this).animate({ color: "white" }, { duration: 200, queue: false});
   });
@@ -13,17 +13,17 @@ function centerVertical(parent, subject) {
   $(parent).css( "paddingTop", 0); //resets size of div
   var parent_height = $(parent).height();
   var subject_height = $(subject).outerHeight();
-  
+
   var padding_amount = (parent_height - subject_height) / 2;
-  
+
   if ($(".is-home-page").height()){ // is a dirty fix for mobile, tried using a script to adjust height but typekit loading fucks with it. Only loads on homepage
     console.log("is home page");
     padding_amount = padding_amount;
   }
-  
+
   console.log("subject height: " + subject_height);
   console.log("parent height: " + parent_height);
-  $(parent).css( "paddingTop", padding_amount); 
+  $(parent).css( "paddingTop", padding_amount);
 }
 
 function isMobile() {
@@ -35,12 +35,21 @@ function isMobile() {
 
 $(document).ready(function(){
 
+  if ($(".index-screen").length > 0) {
+    $(".index-screen").css({"display": "none"})
+
+     $(window).on("load", function(){
+       $(".index-screen").fadeIn();
+     })
+
+  }
+
   hoverColor(".facebook", "#3b5998");
   hoverColor(".twitter", "#4099ff");
   hoverColor(".youtube", "#cc181e");
-  
+
   var menu_open = false;
-  
+
   $(".hamburger-container").click(function(){
     if (menu_open == false) {
       $(".navbar-links").slideDown();
@@ -51,16 +60,17 @@ $(document).ready(function(){
       menu_open = false;
     }
   })
-  
+
   if (isMobile()) {
 
     centerVertical(".index-screen", ".center-container"); //home page text
     centerVertical(".cover-picture", ".center-text-page"); //home page text
-    
-   
+
+
 
   }
-  
- 
+
+
 })
+
 },{}]},{},[1]);
